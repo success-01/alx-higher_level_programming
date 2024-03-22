7e7d0bc36738beef3af637589f8f5b45033cec64
success-01 <nompumeleloxaba972@gmail.com>
2024-03-22 06:36:10 -0700

    positive negative

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * check_cycle -checks if linked list contains a cycle
+ *@list:list to check
+ *Return:1 if list as cycle,0 if it doesnt
+ */
+ int check_cycle(listint_t *list)
+{
+
+}
